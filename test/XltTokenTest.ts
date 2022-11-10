800f64cc484d8a3d250493c179c02965322ac678
JAE YEON KIM <65452447+jaethewiederholen@users.noreply.github.com>
2022-11-10 21:18:55 +0900

    fix test code

--- test/XltTokenTest.ts
+++ test/XltTokenTest.ts
@@ -1,27 +1,25 @@
-import {expect} from "chai";
-import {ethers} from "hardhat";
+import { expect } from "chai";
+import { ethers } from "hardhat";
 import { XltToken } from "../typechain-types";
 
-let xltToken : XltToken;
+async function deployContracts() {
+    const [deployer, user] = await ethers.getSigners();
 
-const deployedContract :string = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"
+    // #1. Deploy Xlt Token Contract
+    const XltToken = await ethers.getContractFactory("XltToken")
+    const xltToken = await XltToken.connect(deployer).deploy();
+    await xltToken.deployed();
+    return { deployer, user, xltToken }
+}
 
-describe("Get instance of xltToken", function () {
-    before(async function() {
-        xltToken = await ethers.getContractAt("XltToken", deployedContract) as XltToken
-    })
-    it("user get right amount of xlt when they buy them", async function() {
-        const signers = await ethers.getSigners();
-        const buyerAddress = signers[5].address;
-        const signer = await ethers.getSigner(buyerAddress);
+describe("Buy Xlt token", function () {
+    it("user get right amount of xlt when they buy them", async function () {
+        const { deployer, user, xltToken } = await deployContracts();
 
-        const option = {value: ethers.utils.parseEther("2")}
-        const buy: any = await xltToken.connect(signer).buyXlt(option)
-        const tx = await buy.wait();
-        const amount = tx.events[0].args[0];
-        const buyer = tx.events[0].args[1];
-        console.log(await xltToken.balanceOf(buyer))
+        const option = { value: ethers.utils.parseEther("2") }
+        const buy: any = await xltToken.connect(user).buyXlt(option)
+        await buy.wait();
 
-        expect(Number(await xltToken.balanceOf(buyerAddress))).to.equal(2*0.2047073300*1e18)
+        expect(Number(await xltToken.balanceOf(user.address))).to.equal(2 * 0.2047073300 * 1e18)
     })
 })
